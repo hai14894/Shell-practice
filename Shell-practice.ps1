c27918e88b54438e5aa41266bb7d66288ae19b2a
hai14894 <hohoanghai14894@gmail.com>
2020-11-24 22:08:26 +1100

    do while loop

--- Shell-practice.ps1
+++ Shell-practice.ps1
@@ -1,4 +1,4 @@
-
+#count Large Files
 $path = Read-Host -Prompt 'Please enter the file path you wish to scan for large file' 
 $rawFileData = Get-ChildItem -Path $path -Recurse
 $largeFiles = $rawFileData | Where-Object { $_.Length -gt 5MB }
@@ -6,6 +6,7 @@ $largeFilesCount = $largeFiles | Measure-Object | Select-Object -ExpandProperty 
 
 Write-Host "you have $largeFilesCount large file(s) in $path"
 ################################################################
+# if else condition
 $path = 'Downloads'
 $testPath = Test-Path $path
 if ($testPath -eq $true) {
@@ -15,7 +16,7 @@ else {
     Write-Host "$path is not VERIFIED "
 }
 ########################################################################
-
+# for each loop
 $path = 'Downloads'
 [int]$totalSize = ''
 $fileInfo = Get-ChildItem $path -Recurse
@@ -23,4 +24,15 @@ foreach ($file in $fileInfo) {
     $totalSize += $file.Length
 }
 $roundedSize = [math]::Round($($totalSize / 1MB))
-Write-Host " the toal space disk of this directory is $roundedSize MB"
+Write-Host " the toal space disk of $path is $roundedSize MB"
+
+##################################
+#do while loop 
+$pathVerified = $false
+do {
+    $path =  Read-Host 'Please enter a path to verify'
+    if ( Test-Path $path ) {
+        Write-Host " Path is verified"
+        $pathVerified = $true
+    }
+} while ($pathVerified -eq $false)
