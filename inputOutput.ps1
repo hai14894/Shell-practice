46201126926839c3a214ed68f6fae8ddab74990d
hai14894 <hohoanghai14894@gmail.com>
2020-11-26 20:47:16 +1100

    git number of posts by user input

--- inputOutput.ps1
+++ inputOutput.ps1
@@ -1,5 +1,9 @@
 $webResult = Invoke-WebRequest -Uri 'http://reddit.com/r/powershell.json'
 $rawJSON = $webResult.Content
-$objData = $rawJSON | ConvertFrom-Json
+$objData = $rawJSON |    ConvertFrom-Json
 $posts = $objData.data.children.data
 $posts | Select-Object Title,Score | Sort-Object Score -Descending
+
+################################################################
+[int]$numPosts = Read-Host -Prompt "Enter the number of post you want to read"
+$posts | Select-Object Title,url | Sort-Object Score -Descending | Select-Object -First $numPosts
